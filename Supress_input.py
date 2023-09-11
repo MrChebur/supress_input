4b5425638f140ae7d812d543c53aafc1914f48b5
mrChebur <mrCheburPlugins@inbox.ru>
2023-09-11 23:04:27 +0500

    master unblock buttons

--- Supress_input.py
+++ Supress_input.py
@@ -1,37 +1,40 @@
-import time
-import pynput  # https://github.com/moses-palmer/pynput
-from tqdm import tqdm
+from pynput import keyboard, mouse  # https://github.com/moses-palmer/pynput
 
 
 class SupressInput:
     def __init__(self):
-        self.mouse_listener = pynput.mouse.Listener(suppress=True)
-        self.keyboard_listener = pynput.keyboard.Listener(suppress=True)
+        self.key_combination = '<ctrl>+h'
+        self.hotkey = keyboard.HotKey(keyboard.HotKey.parse(self.key_combination), self.enable_all)
 
     def disable_all(self):
         """Disable mouse and keyboard events"""
-        self.mouse_listener.start()
-        self.keyboard_listener.start()
+
+        with mouse.Listener(suppress=True) as self.mouse_listener, \
+                keyboard.Listener(suppress=True,
+                                  on_press=self.for_canonical(self.hotkey.press),
+                                  on_release=self.for_canonical(self.hotkey.release)) as self.keyboard_listener:
+            self.mouse_listener.join()
+            self.keyboard_listener.join()
+
+    def show_unblock_message(self):
+        print()
+        print(f'Push {self.key_combination} to unblock mouse and keyboard input...')
 
     def enable_all(self):
         """Enable mouse and keyboard events"""
         self.mouse_listener.stop()
         self.keyboard_listener.stop()
+        return False
 
-    def wait(self, seconds_to_sleep=10):
-        """Suppress input for X seconds"""
+    def for_canonical(self, f):
+        return lambda k: f(self.keyboard_listener.canonical(k))
 
-        self.disable_all()
-        print('Input disabled!')
-        print(f'Sleeping {seconds_to_sleep} seconds...')
 
-        for x in tqdm(range(1, seconds_to_sleep + 1)):
-            time.sleep(1)
-
-        self.enable_all()
-        print('Input enabled!')
+def main():
+    supress_input = SupressInput()
+    supress_input.show_unblock_message()
+    supress_input.disable_all()
 
 
 if __name__ == '__main__':
-    supress_input = SupressInput()
-    supress_input.wait(3)
+    main()
