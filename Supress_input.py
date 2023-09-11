4cca22677b6a5a087d88a795ca331832c10a7690
mrChebur <mrCheburPlugins@inbox.ru>
2023-09-11 20:29:42 +0500

    master

--- Supress_input.py
+++ Supress_input.py
@@ -8,35 +8,30 @@ class SupressInput:
         self.mouse_listener = pynput.mouse.Listener(suppress=True)
         self.keyboard_listener = pynput.keyboard.Listener(suppress=True)
 
-    def disable(self):
+    def disable_all(self):
         """Disable mouse and keyboard events"""
         self.mouse_listener.start()
         self.keyboard_listener.start()
 
-    def enable(self):
+    def enable_all(self):
         """Enable mouse and keyboard events"""
         self.mouse_listener.stop()
         self.keyboard_listener.stop()
 
-    def for_seconds(self, seconds_to_sleep=10):
-        """Suppress input for X seconds and print a message every second"""
+    def wait(self, seconds_to_sleep=10):
+        """Suppress input for X seconds"""
 
-        self.disable()
+        self.disable_all()
         print('Input disabled!')
         print(f'Sleeping {seconds_to_sleep} seconds...')
 
-        bar_format = '{desc:<5.5}{percentage:3.0f}%|{bar:10}{r_bar}'
-
-        for x in tqdm(
-                range(1, seconds_to_sleep + 1),
-                bar_format=bar_format):
-            # print(f'Sleeping {x} of {seconds_to_sleep} seconds...')
+        for x in tqdm(range(1, seconds_to_sleep + 1)):
             time.sleep(1)
 
-        self.enable()
+        self.enable_all()
         print('Input enabled!')
 
 
 if __name__ == '__main__':
-    supress = SupressInput()
-    supress.for_seconds(3)
+    supress_input = SupressInput()
+    supress_input.wait(3)
